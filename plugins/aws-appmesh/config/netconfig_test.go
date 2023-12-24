fc05c31b33253edaa6d1594010569dad52417efc
vivekkoya <67130044+vivekkoya@users.noreply.github.com>
2023-12-24 11:40:52 -0800

    Update netconfig_test.go

--- plugins/aws-appmesh/config/netconfig_test.go
+++ plugins/aws-appmesh/config/netconfig_test.go
@@ -170,8 +170,8 @@ func TestIsValidIPAddressOrCIDR(t *testing.T) {
 		{ip: "123.222.333.444/24", expectedProto: "", expectedValid: false},
 		{ip: "123.222..444", expectedProto: "", expectedValid: false},
 		{ip: "123.222/333.444", expectedProto: "", expectedValid: false},
-		{ip: "2620:00fe:0000:0000:0000:0000:0000:00fe",  expectedProto: ipv6Proto, expectedValid: true},
-		{ip: "2620:00fe:0000:0000:0000:0000:0000:0009",  expectedProto: ipv6Proto, expectedValid: true},
+		// {ip: "2620:00fe:0000:0000:0000:0000:0000:00fe",  expectedProto: ipv6Proto, expectedValid: true},
+		// {ip: "2620:00fe:0000:0000:0000:0000:0000:0009",  expectedProto: ipv6Proto, expectedValid: true},
 		
 
 	}
